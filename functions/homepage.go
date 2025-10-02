0a45a300f1d6a0818868f1fbbc054b31759e4e7a
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-02 14:27:50 +0100

    debuggin

--- functions/homepage.go
+++ functions/homepage.go
@@ -1,7 +1,31 @@
 package groupino
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
-func HomePage(w http.ResponseWriter, r *http.Request){
+const url string = "https://groupietrackers.herokuapp.com/api/artists"
 
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w, "4000 bad request en methode", http.StatusBadRequest)
+		return
+	}
+	temp, err := template.ParseFiles("templates/pagehome.html")
+	if err != nil {
+		http.Error(w, "500 internale server error en parsfile", http.StatusInternalServerError)
+		return
+	}
+	var Slicedata Data
+	err1 := FetchData(url, &Slicedata.Artists)
+	if err1 != nil {
+		http.Error(w, "500 internal server error ,, error en fetch data", http.StatusInternalServerError)
+		return
+	}
+	temp.Execute(w, Slicedata.Artists)
 }

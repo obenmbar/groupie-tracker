5304d2bff627918f922cdb02076dbc19f3ed403a
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-02 14:28:35 +0100

    debuggin

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	groupino "groupino/functions"
 	"log"
 	"net/http"
@@ -8,8 +9,9 @@ import (
 
 func main() {
 	http.HandleFunc("/",groupino.HomePage)
-	http.HandleFunc("/artist", groupino.ArtistPage)
-	http.HandleFunc("/style",groupino.Style)
+	http.HandleFunc("/artist/", groupino.ArtistPage)
+	http.HandleFunc("/style/",groupino.Style)
+	fmt.Println("http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)					
 }

a4e37a96136e79dbff5ac52c702d8331b2470128
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-02 14:26:43 +0100

    debugging

--- functions/artistpage.go
+++ functions/artistpage.go
@@ -1,7 +1,30 @@
 package groupino
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 func ArtistPage(w http.ResponseWriter, r *http.Request) {
-	
+	if r.Method != http.MethodGet {
+		http.Error(w, "405-methode not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	id := r.URL.Query().Get("id")
+	Id, erratoi := strconv.Atoi(id)
+	if erratoi != nil {
+		fmt.Println("iuezgfuyezgf")
+	}
+	url := fmt.Sprintf("https://groupietrackers.herokuapp.com/api/artists/%d", Id)
+	var artist artist
+	err1 := FetchData(url, &artist)
+	if err1 != nil {
+		fmt.Println(err1)
+		http.Error(w, "500 internal server error ,, error en fetch data", http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(artist)
+	//locurl:=artist.Locations
+
 }

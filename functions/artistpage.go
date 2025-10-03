efda78cc4701cec8125ac92eccfb6ded782acd1d
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-03 12:23:21 +0100

    debuuging

--- functions/artistpage.go
+++ functions/artistpage.go
@@ -2,8 +2,15 @@ package groupino
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
+)
+
+var (
+	Locations   Location
+	Concertdate Date
+	Relations   Relation
 )
 
 func ArtistPage(w http.ResponseWriter, r *http.Request) {
@@ -13,18 +20,56 @@ func ArtistPage(w http.ResponseWriter, r *http.Request) {
 	}
 	id := r.URL.Query().Get("id")
 	Id, erratoi := strconv.Atoi(id)
-	if erratoi != nil {
-		fmt.Println("iuezgfuyezgf")
+	fmt.Println(Id)
+	if erratoi != nil || Id < 1 || Id > 52 {
+		http.Error(w, "404 not found id ", http.StatusNotFound)
+		return
 	}
 	url := fmt.Sprintf("https://groupietrackers.herokuapp.com/api/artists/%d", Id)
-	var artist artist
+	var artist Artist
 	err1 := FetchData(url, &artist)
 	if err1 != nil {
-		fmt.Println(err1)
 		http.Error(w, "500 internal server error ,, error en fetch data", http.StatusInternalServerError)
 		return
 	}
-	fmt.Println(artist)
-	//locurl:=artist.Locations
 
+	location := artist.Locations
+	concertdate := artist.ConcertDates
+	relatio := artist.Relations
+	fmt.Println(relatio)
+	err2 := FetchData(location, &Locations)
+	if err2 != nil {
+		http.Error(w, "500 internal server error en fetch data de location", http.StatusInternalServerError)
+		return
+	}
+	err3 := FetchData(concertdate, &Concertdate)
+	if err3 != nil {
+		http.Error(w, "500 internal server error en fetch data de concert date ", http.StatusInternalServerError)
+		return
+	}
+	err4 := FetchData(relatio, &Relations)
+	if err4 != nil {
+		http.Error(w, "500 internal server error en fetch data de relation", http.StatusInternalServerError)
+		return
+	}
+
+	GlobalStruct := Global{
+		Artist:   artist,
+		Date:     Concertdate,
+		Location: Locations,
+		Relation: Relations,
+	}
+	tempp, err := template.ParseFiles("templates/pageartist.html")
+	if err != nil {
+		fmt.Println("aaaa")
+		http.Error(w, "500 internale server error en parse file artist ", http.StatusInternalServerError)
+		return
+	}
+
+	err = tempp.Execute(w, GlobalStruct)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "500 internale server error en execute artist ", http.StatusInternalServerError)
+		return
+	}
 }

e2a9caa4e52cff78d8527aa45819da92875a5e65
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-10 11:13:05 +0100

    debbuging

--- functions/homepage.go
+++ functions/homepage.go
@@ -9,24 +9,28 @@ const url string = "https://groupietrackers.herokuapp.com/api/artists"
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.Error(w, "404 not found", http.StatusNotFound)
+		RendError(w, "404 not found", http.StatusNotFound)
 		return
 	}
 	if r.Method != http.MethodGet {
-		http.Error(w, "4000 bad request en methode", http.StatusBadRequest)
+		RendError(w, "4000 bad request en methode", http.StatusBadRequest)
 		return
 	}
 	temp, err := template.ParseFiles("templates/pagehome.html")
 	if err != nil {
-		http.Error(w, "500 internale server error en parsfile", http.StatusInternalServerError)
+		RendError(w, "500 internale server error, error en parsfile", http.StatusInternalServerError)
 		return
 	}
 	var Slicedata Data
 	err1 := FetchData(url, &Slicedata.Artists)
 	if err1 != nil {
-		http.Error(w, "500 internal server error ,, error en fetch data", http.StatusInternalServerError)
+		RendError(w, "500 internal server error , error en fetch data", http.StatusInternalServerError)
 		return
 	}
 
-	temp.Execute(w, Slicedata.Artists)
+	err = temp.Execute(w, Slicedata.Artists)
+	if err != nil {
+		RendError(w, "500 internal server error ", http.StatusInternalServerError)
+		return
+	}
 }

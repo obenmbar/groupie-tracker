3bcab523bcb5f557bea022599b74fca481372fe2
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-10 11:13:30 +0100

    debbuging

--- functions/style.go
+++ functions/style.go
@@ -8,11 +8,11 @@ import (
 func Style(w http.ResponseWriter, r *http.Request) {
 	fileinfo, err := os.Stat(r.URL.Path[1:])
 	if err != nil {
-		http.Error(w,"403 forbidden", http.StatusForbidden)
+		RendError(w,"403 forbidden", http.StatusForbidden)
 		return
 	}
 	if fileinfo.IsDir(){
-		http.Error(w,"403 forbiden tu ne peux pas acceder a cette file",http.StatusForbidden)
+		RendError(w,"403 forbiden tu ne peux pas acceder a cette file",http.StatusForbidden)
 		return
 	}
 	http.ServeFile(w, r, r.URL.Path[1:])
